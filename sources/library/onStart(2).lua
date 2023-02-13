9663da41981727bc672690d77a1057c9cf3e2ca4
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2023-02-12 22:45:53 -0400

    Update onStart(2).lua
    Tweak for expanded diagnostics data

--- sources/library/onStart(2).lua
+++ sources/library/onStart(2).lua
@@ -2,46 +2,58 @@
 -- define key functions for use elsewhere
 ---
 function UpdateIndustry(table_IndustryList)
-  local lclIndustryStatusData = {}
+    local lclIndustryStatusData = {}
 
-  for i = 1, #table_IndustryList, 1 do
-    local aRecord = {}
-    if table_IndustryList[i] ~= nil then 
-          
-        local status_table = table_IndustryList[i].getInfo()
-        otherComments = "simple test data."
+    for i = 1, #table_IndustryList, 1 do
+      local aRecord = {}
+      if table_IndustryList[i] ~= nil then 
+            
+          local status_table = table_IndustryList[i].getInfo()
 
-        aRecord["machineName"] = table_IndustryList[i].getName()
-        local product_id = status_table.currentProducts[1].id
-        aRecord["product_name"] = system.getItem(product_id).locDisplayNameWithSize
-        aRecord["status_code"]  = status_table.state
-        aRecord["otherComments"] = otherComments
-          
-        lclIndustryStatusData[i] = aRecord
-        end --- if machinery
-    end   --- for i 
+          aRecord["machineName"] = table_IndustryList[i].getName()
+          local product_id = status_table.currentProducts[1].id
+          aRecord["product_name"] = system.getItem(product_id).locDisplayNameWithSize
+          aRecord["product_iconPath"] = system.getItem(product_id).iconPath
+          aRecord["status_code"]  = status_table.state
+          aRecord["otherComments"] = "---"
+            
+          if aRecord["status_code"] == 1 then
+             aRecord["otherComments"] = status_table.batchesRemaining
+                .. " batches Remaining"
+                .. " of "
+                .. status_table.batchesRequested
+                .. " batches Requested."
+             end --- if aRecord["status_code"]  
+            
+          if aRecord["status_code"] == 2 then
+             aRecord["otherComments"] = "time Remaining on current run: " .. status_table.remainingTime
+             end --- if aRecord["status_code"]
 
-  return lclIndustryStatusData
-  end --- function
+          if aRecord["status_code"] == 6 
+             and status_table.maintainProductAmount > 0
+             then
+                 aRecord["otherComments"] = "maintain Product Amount: " .. status_table.maintainProductAmount
+                 end --- if aRecord["status_code"]
+            
+          lclIndustryStatusData[i] = aRecord
+          end --- if machinery
+      end   --- for i 
+  
+    return lclIndustryStatusData
+    end --- function
 
 ---
 function UpdateScreens(lcl_ScreenList, lcl_IndustryStatusData)
----system.print(wss_software.id 
----              .. "|lcl_ScreenList #"
----              .. #lcl_ScreenList
----      )
  
-for i = 1, #lcl_ScreenList, 1 do
-      ---system.print(wss_software.id 
-      ---  .. lcl_ScreenList[i].getName()
-      ---)
-      renderScreen(
-          lcl_ScreenList[i], 
-          lcl_IndustryStatusData[i].machineName, 
-          lcl_IndustryStatusData[i].product_name, 
-          lcl_IndustryStatusData[i].otherComments, 
-          lcl_IndustryStatusData[i].status_code
-      )
-   end
-end --- function
+  for i = 1, #lcl_ScreenList, 1 do
+        renderScreen(
+            lcl_ScreenList[i], 
+            lcl_IndustryStatusData[i].machineName, 
+            lcl_IndustryStatusData[i].product_name,
+            lcl_IndustryStatusData[i].otherComments, 
+            lcl_IndustryStatusData[i].status_code,
+            lcl_IndustryStatusData[i].product_iconPath
+        )
+     end
+  end --- function
 --- eof ---
