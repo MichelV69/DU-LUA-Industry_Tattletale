4f67a5c2421822b99c1d1b6bb7a358b50a9837ae
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2023-02-15 20:41:31 -0400

    Improving output on screen for long job times

--- sources/library/onStart(2).lua
+++ sources/library/onStart(2).lua
@@ -2,58 +2,67 @@
 -- define key functions for use elsewhere
 ---
 function UpdateIndustry(table_IndustryList)
-    local lclIndustryStatusData = {}
+   local lclIndustryStatusData = {}
 
-    for i = 1, #table_IndustryList, 1 do
+   for i = 1, #table_IndustryList, 1 do
       local aRecord = {}
-      if table_IndustryList[i] ~= nil then 
-            
-          local status_table = table_IndustryList[i].getInfo()
+      if table_IndustryList[i] ~= nil then
+         local status_table          = table_IndustryList[i].getInfo()
 
-          aRecord["machineName"] = table_IndustryList[i].getName()
-          local product_id = status_table.currentProducts[1].id
-          aRecord["product_name"] = system.getItem(product_id).locDisplayNameWithSize
-          aRecord["product_iconPath"] = system.getItem(product_id).iconPath
-          aRecord["status_code"]  = status_table.state
-          aRecord["otherComments"] = "---"
-            
-          if aRecord["status_code"] == 1 then
-             aRecord["otherComments"] = status_table.batchesRemaining
+         aRecord["machineName"]      = table_IndustryList[i].getName()
+         local product_id            = status_table.currentProducts[1].id
+         aRecord["product_name"]     = system.getItem(product_id).locDisplayNameWithSize
+         aRecord["product_iconPath"] = system.getItem(product_id).iconPath
+         aRecord["status_code"]      = status_table.state
+         aRecord["otherComments"]    = "---"
+
+         if aRecord["status_code"] == 1 then
+            aRecord["otherComments"] = status_table.batchesRemaining
                 .. " batches Remaining"
                 .. " of "
                 .. status_table.batchesRequested
                 .. " batches Requested."
-             end --- if aRecord["status_code"]  
-            
-          if aRecord["status_code"] == 2 then
-             aRecord["otherComments"] = "time Remaining on current run: " .. status_table.remainingTime
-             end --- if aRecord["status_code"]
+         end --- if aRecord["status_code"]
 
-          if aRecord["status_code"] == 6 
+         if aRecord["status_code"] == 2 then
+            aRecord["otherComments"] = "time Remaining on current run: " .. TimeLeftToHuman(status_table.remainingTime)
+         end --- if aRecord["status_code"]
+
+         if aRecord["status_code"] == 6
              and status_table.maintainProductAmount > 0
-             then
-                 aRecord["otherComments"] = "maintain Product Amount: " .. status_table.maintainProductAmount
-                 end --- if aRecord["status_code"]
-            
-          lclIndustryStatusData[i] = aRecord
-          end --- if machinery
-      end   --- for i 
-  
-    return lclIndustryStatusData
-    end --- function
+         then
+            aRecord["otherComments"] = "maintain Product Amount: " .. status_table.maintainProductAmount
+         end --- if aRecord["status_code"]
+
+         lclIndustryStatusData[i] = aRecord
+      end --- if machinery
+   end --- for i
+
+   return lclIndustryStatusData
+end --- function
 
 ---
 function UpdateScreens(lcl_ScreenList, lcl_IndustryStatusData)
- 
-  for i = 1, #lcl_ScreenList, 1 do
-        RenderScreen(
-            lcl_ScreenList[i], 
-            lcl_IndustryStatusData[i].machineName, 
-            lcl_IndustryStatusData[i].product_name,
-            lcl_IndustryStatusData[i].otherComments, 
-            lcl_IndustryStatusData[i].status_code,
-            lcl_IndustryStatusData[i].product_iconPath
-        )
-     end
-  end --- function
+   for i = 1, #lcl_ScreenList, 1 do
+      RenderScreen(
+         lcl_ScreenList[i],
+         lcl_IndustryStatusData[i].machineName,
+         lcl_IndustryStatusData[i].product_name,
+         lcl_IndustryStatusData[i].otherComments,
+         lcl_IndustryStatusData[i].status_code,
+         lcl_IndustryStatusData[i].product_iconPath
+      )
+   end
+end --- function
+
+function TimeLeftToHuman(timeInSeconds)
+   local floor = math.floor()
+   local format = string.format()
+   local days = floor(time / 86400)
+   local hours = floor(mod(time, 86400) / 3600)
+   local minutes = floor(mod(time, 3600) / 60)
+   local seconds = floor(mod(time, 60))
+   return format("%d:%02d:%02d:%02d", days, hours, minutes, seconds)
+end
+
 --- eof ---
